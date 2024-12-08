cba7ca3089ac473ba32b4e59fcc4f3bb444c0156
ergz <erodriguez@hey.com>
2024-12-08 01:04:42 -0800

    initial

---
+++ tailwind.config.ts
@@ -0,0 +1,15 @@
+import typography from '@tailwindcss/typography';
+import type { Config } from 'tailwindcss';
+
+export default {
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+
+  theme: {
+    extend: {}
+  },
+
+  plugins: [
+    typography,
+    require("daisyui")
+  ]
+} satisfies Config;
