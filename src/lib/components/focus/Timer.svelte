b10b1a6b6fc52a19c95e2d69d446a2d92ab3775c
ergz <erodriguez@hey.com>
2024-12-09 23:09:15 -0800

    work

---
+++ src/lib/components/focus/Timer.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+	let current_clock_time = $state("");
+	let timer_active = $state(false);
+	let focus_time = $state("30");
+
+	$inspect(focus_time);
+
+	function update_clock() {
+		current_clock_time = new Date().toLocaleString();
+	}
+
+	function toggle_timer_active() {
+		timer_active = !timer_active;
+	}
+
+	$effect(() => {
+		update_clock();
+		const interval = setInterval(update_clock, 1000);
+
+		return () => clearInterval(interval);
+	});
+</script>
+
+<div class="text-3xl">{current_clock_time}</div>
+<div>
+	<select class="select select-lg w-full max-w-xs" bind:value={focus_time}>
+		<option value="30">30 minutes</option>
+		<option value="45">45 minutes</option>
+		<option value="60">1 hour</option>
+		<option value="120">2 hours</option>
+	</select>
+	<button class="btn {timer_active ? 'btn-error' : 'btn-success'}" onclick={toggle_timer_active}>
+		{timer_active ? "Stop" : "Start"}</button
+	>
+	<h1>Focus for {focus_time} minutes</h1>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,7 @@
+<script lang="ts">
+	import Timer from "$lib/components/focus/Timer.svelte";
+</script>
+
 <div class="mx-auto h-screen w-[60%]">
 	<div class="navbar bg-base-300">
 		<div class="navbar-start">
@@ -29,25 +33,9 @@
 			</div>
 		</div>
 		<div class="navbar-center">
-			<a class="btn btn-ghost text-xl">focusnow</a>
+			<a class="btn btn-ghost text-xl">focus log</a>
 		</div>
 		<div class="navbar-end">
-			<!-- <button class="btn btn-circle btn-ghost"> -->
-			<!-- 	<svg -->
-			<!-- 		xmlns="http://www.w3.org/2000/svg" -->
-			<!-- 		class="h-5 w-5" -->
-			<!-- 		fill="none" -->
-			<!-- 		viewBox="0 0 24 24" -->
-			<!-- 		stroke="currentColor" -->
-			<!-- 	> -->
-			<!-- 		<path -->
-			<!-- 			stroke-linecap="round" -->
-			<!-- 			stroke-linejoin="round" -->
-			<!-- 			stroke-width="2" -->
-			<!-- 			d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" -->
-			<!-- 		/> -->
-			<!-- 	</svg> -->
-			<!-- </button> -->
 			<button class="btn btn-circle btn-ghost">
 				<div class="indicator">
 					<svg
@@ -69,5 +57,32 @@
 			</button>
 		</div>
 	</div>
+
+	<div class="flex h-screen">
+		<div class=" w-2/3 flex-col">
+			<div class="top-div h-[200px]"><Timer /></div>
+			<div class="left-div flex-grow">
+				Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
+				been the industry's standard dummy text ever since the 1500s, when an unknown printer took a
+				galley of type and scrambled it to make a type specimen book. It has survived not only five
+				centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+				It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum
+				passages, and more recently with desktop publishing software like Aldus PageMaker including
+				versions of Lorem Ipsum.
+			</div>
+		</div>
+		<div class="right-div h-full w-1/3">Music/media stuff here</div>
+	</div>
 </div>
 
+<style>
+	.top-div {
+		background: purple;
+	}
+	.left-div {
+		background: green;
+	}
+	.right-div {
+		background: blue;
+	}
+</style>
